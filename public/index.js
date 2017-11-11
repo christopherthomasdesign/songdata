d85fc9e4692962adb7c879c54c5a725e098ff5f3
pataruco <pataruco@gmail.com>
2017-11-11 00:10:34 +0000

    Rendering spotify response on brwoser

--- public/index.js
+++ public/index.js
@@ -1,3 +1,13 @@
 $(window).on("load",  (e) =>  {
+  const settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "http://localhost:3000/spotify-data/audio-features/4cGeMgLcykDLAazHFpbZbU",
+    "method": "GET",
+  }
   
+  $.ajax(settings).done( (response) =>  {
+    console.log(response);
+    $('#js-code').text( JSON.stringify(response) )
+  });
 });
