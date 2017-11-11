8850f53ee912fec861b7ed8af7b9177ae128d489
pataruco <pataruco@gmail.com>
2017-11-11 00:05:21 +0000

    Pull data from Spotify and send to an endpoint

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const path = require('path');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const controllers = require('./controllers/index');
+
+//Middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/', express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+//Router
+app.use('/', controllers);
+
+//Server console
+app.use((req, res, next) => {
+    console.log('%s request to %s from %s', req.method, req.path, req.ip);
+    next();
+});
+
+//Server
+const server = app.listen(port, () => {
+    console.log(`songdata is working on port ${port}`);
+});
+
+module.exports = server;
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,28 @@
+const express = require('express');
+const router = express.Router();
 require('dotenv').config()
 const axios = require('axios');
 
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.SECRET_ID;
+
+router.get('/', (req, res) => {
+  res.render('index');
+});
+
+router.get('/spotify-data/:endpoint/:request', (req, res) => {
+  const spotifyQuery = `${req.params.endpoint}/${req.params.request}`;
+  
+  getSpotifyData( spotifyQuery )
+  .then( response =>  {
+    return res.status(200).send(response.data)
+  }).catch( error =>  {
+    throw new Error(error);
+  });
+});
+
+
+
 
 function getToken( ) {
   return axios({
@@ -24,24 +44,13 @@ function getToken( ) {
 async function getSpotifyData( endpoint ){
   const tokenData = await getToken( );
   const token = tokenData.data.access_token;
-  axios({
+  return axios({
     url: `https://api.spotify.com/v1/${endpoint}`,
     method: 'get',
     headers: {
       'Authorization': 'Bearer ' + token
     }
-  }).then( response =>  {
-    console.log( response.data );
-    return response.data;
-  }).catch( error =>  {
-    throw new Error(error);
-  });
+  })
 }
 
-module.expors = getSpotifyData;
-
-// run function to get spotify data, with the endpoint as the argument
-// so this should happen in response to a user action such as a search
-
-// getSpotifyData( 'audio-features/4cGeMgLcykDLAazHFpbZbU' );
-// getSpotifyData( 'tracks/4cGeMgLcykDLAazHFpbZbU' );
+module.exports = router;
